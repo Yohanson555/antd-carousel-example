cbc1e4a87163a5a49fc23fcf1226580d3e1e6523
Yohanson555 <yohanson555@gmail.com>
2022-06-15 21:34:34 +0300

    misc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import { Carousel } from 'antd';
+
 import './App.css';
 
-function App() {
+const containerStyle = {
+  margin: '20px auto',
+  height: "80vh",
+  width: "90%",
+  backgroundColor: "#cdcdcd",
+  position: "relative",
+};
+
+const contentStyle = {
+  height: '100%',
+  width: '100%',
+  color: '#fff',
+  textAlign: 'center',
+  background: '#364d79',
+  display: "flex",
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+
+const App = () => {
+  const ref = useRef();
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
+
+  const goNext = () => {
+    if (ref.current) {
+      ref.current.next();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle} onClick={goNext}>
+      <Carousel
+        ref={ref}
+        afterChange={onChange}
+        dots={false}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
     </div>
   );
-}
+};
 
 export default App;
